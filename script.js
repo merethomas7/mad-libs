a9258593f50c7ae427a2038c63bc29db784f1fc4
merethomas7 <merediththomas972@gmail.com>
2017-07-20 13:15:15 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-function myFunction() {
-  var x = document.getElementById("fname").value;
+
+
+function speech() {
+    var puppy = document.getElementById("myText").value;
+    document.getElementById("adjective1").innerHTML=puppy;
+
+    var puppy2 = document.getElementById("myText2").value;
+    document.getElementById("adjective2").innerHTML=puppy2
+
+    var puppy3 = document.getElementById("myText3").value;
+    document.getElementById("name").innerHTML=puppy3
+
+    var puppy4 = document.getElementById("myText4").value;
+    document.getElementById("verbing").innerHTML=puppy4
+
+    var puppy5 = document.getElementById("myText5").value;
+    document.getElementById("verb").innerHTML=puppy5
+
+    var puppy6 = document.getElementById("myText6").value;
+    document.getElementById("noun").innerHTML=puppy6
+
+      document.getElementById('para').style.display='block';
+
 }
